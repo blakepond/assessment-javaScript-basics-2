014d9991ad98574f2f247f9432e8b38b69c3514e
Blake Pond <blakehp@icloud.com>
2021-10-22 19:57:22 -0600

    orders complete

--- orders.js
+++ orders.js
@@ -29,7 +29,19 @@
     stored on the object.
 */
 
-//CODE HERE
+class Ticket {
+    constructor (items, orderTime, customerID) {
+        this.items = items
+        this.orderTime = orderTime
+        this.customerID = customerID
+        this.status = "queued"
+    }
+
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerID} is now ${this.status}`)
+    }
+}
 
 
 
@@ -43,7 +55,7 @@
     ordered at: 7:03 PM
     customer: 575
 */
-
+let firstTicket = new Ticket("pizza, bread and soda", "7:03 PM", 575)
 //CODE HERE
 
 
@@ -53,4 +65,4 @@
     'cooking'
 */
 
-//CODE HERE
+firstTicket.updateStatus("cooking")
